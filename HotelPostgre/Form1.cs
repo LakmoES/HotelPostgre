f0c25a208985416acf9022f9d303f75227905c3f
lakmoes <mokinous@gmail.com>
2016-03-25 19:09:08 +0200

    init

---
+++ HotelPostgre/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace HotelPostgre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                method();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void method()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=root;Database=Shmelyov;");
+            conn.Open();
+
+            // Define a query
+            NpgsqlCommand cmd = new NpgsqlCommand("select \"HomeBUY\".\"Deal\".\"Dealer\" from \"HomeBUY\".\"Deal\"", conn);
+
+            // Execute a query
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            // Read all rows and output the first column in each row
+            while (dr.Read())
+                Console.Write("{0}\n", dr[0]);
+
+            // Close connection
+            conn.Close();
+        }
+    }
+}
